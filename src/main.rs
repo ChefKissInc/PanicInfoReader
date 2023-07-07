ef0c61491111543f9df4734bfb9192c33b09df90
Visual Ehrmanntraut <30368284+ChefKissInc@users.noreply.github.com>
2023-07-07 17:21:03 +0300

    Uh, 512 bytes is not that much lol
    Signed-off-by: Visual Ehrmanntraut <30368284+ChefKissInc@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn read_from_nvram() -> Option<Vec<u8>> {
     };
     let lpname = PCSTR::from_raw(AAPL_PANIC_INFO.as_ptr());
     let lpguid = PCSTR::from_raw(APPLE_BOOT_VAR_GUID.as_ptr());
-    let mut buf = vec![0u8; 512];
+    let mut buf = vec![0u8; 65476];
     let size = unsafe {
         GetFirmwareEnvironmentVariableA(lpname, lpguid, Some(buf.as_mut_ptr().cast()), size)
     };
