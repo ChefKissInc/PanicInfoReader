6a2d4fddb8a17b7f71c8a11a8688192128cf4a87
Visual <30368284+VisualEhrmanntraut@users.noreply.github.com>
2026-07-01 10:58:51 +0300

    Build configuration fixes

--- xmake.lua
+++ xmake.lua
@@ -1,23 +1,23 @@
 add_rules("mode.debug", "mode.release", "mode.releasedbg")
 
-set_policy("build.optimization.lto", true)
 set_policy("check.target_package_licenses", false)
 
 if is_plat("windows") then
-    add_requires("libplist", {system = false, configs = {toolchains = {"clang-cl"}}})
+	add_requires("libplist", { configs = { toolchains = { "clang-cl" } } })
 else
-    add_requires("libplist", {system = false})
+	add_requires("libplist")
 end
 
 target("PanicInfoReader")
+    if not is_mode("debug") then
+        set_policy("build.optimization.lto", true)
+    end
     set_kind("binary")
     add_files("src/main.cpp")
-    set_languages("c++17")
+    set_languages("cxx17")
     add_packages("libplist")
     if is_plat("macosx") then
         add_frameworks("IOKit", "CoreFoundation")
     elseif is_plat("windows") then
         add_syslinks("advapi32")
     end
-target_end()
-
